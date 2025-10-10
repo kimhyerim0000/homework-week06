06f2c5b58f175303c0834e5af1c17b3072275952
kimhy <kimhyyefla@gmail.com>
2025-10-11 04:08:16 +0900

    feat: Add TodayBanner component to HomeScreen

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/week05/calendar_scheduler/component/main_calendar.dart';
 import 'package:myapp/week05/calendar_scheduler/component/schedule_card.dart';
+import 'package:myapp/week05/calendar_scheduler/component/today_banner.dart';
 
 class HomeScreen extends StatefulWidget {
   // StatelessWidget에서 StatefulWidget으로 전환
