5b62c4a8e42d18232ce55d1701a6a632df600111
kimhy <kimhyyefla@gmail.com>
2025-10-11 04:32:25 +0900

    feat(calendar_scheduler): Implement custom text field component

--- lib/week05/calendar_scheduler/component/custom_text_field.dart
+++ lib/week05/calendar_scheduler/component/custom_text_field.dart
@@ -12,7 +12,17 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(  // 세로로 텍스트와 텍스트 필드 배치
-      
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        TextFormField(), // 폼 안에서 텍스트 필드를 쓸 때 사용
+      ],
     );
   }
 }
