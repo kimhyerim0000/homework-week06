2f73993ab257085fe6c01fa267d1cfa65156a388
kimhy <kimhyyefla@gmail.com>
2025-10-11 04:49:55 +0900

    feat: Implement start time input in schedule bottom sheet

--- lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -19,14 +19,21 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
         child: Padding(
           padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
           child: Column(
-            // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로로 배치
+            // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로로 배치치
             children: [
               Row(
+                // 시작 시간, 종료 시간 가로로 배치치
                 children: [
                   Expanded(
-                    child: CustomTextField(
+                    child: CustomTextField( // 시작 시간 입력 필드드
                       label: '시작 시간',
                       isTime: true,
+                    ),
+                  ),
+                  const SizedBox(width: 16.0),
+                  Expanded(
+                    child: CustomTextField(
+                      
                     )
                   )
                 ],
