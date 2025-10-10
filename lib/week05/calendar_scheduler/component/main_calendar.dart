3b6214864357782b8012e0e3e4ce8984ac98da04
kimhy <kimhyyefla@gmail.com>
2025-10-10 23:53:18 +0900

    feat(calendar): Add comment explaining focusedDay property

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -7,7 +7,7 @@ class MainCalendar extends StatelessWidget {
     return TableCalendar(
       firstDay: DateTime(1800, 1, 1), // 첫째 날
       lastDay: DateTime(3000, 1, 1), // 마지막 날
-      focusedDay: DateTime.now(),
+      focusedDay: DateTime.now(), // 화면에 보여지는 날
     );
   }
 }
