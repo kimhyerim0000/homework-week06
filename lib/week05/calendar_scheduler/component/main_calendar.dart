466beb8a36140c9bef4d104361bd22e639606359
kimhy <kimhyyefla@gmail.com>
2025-10-10 23:56:19 +0900

    feat: Center calendar header title

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 
 class MainCalendar extends StatelessWidget {
   @override
@@ -8,6 +9,9 @@ class MainCalendar extends StatelessWidget {
       firstDay: DateTime(1800, 1, 1), // 첫째 날
       lastDay: DateTime(3000, 1, 1), // 마지막 날
       focusedDay: DateTime.now(), // 화면에 보여지는 날
+      headerStyle: HeaderStyle(
+        titleCentered: true,
+      )
     );
   }
 }
