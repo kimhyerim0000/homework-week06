0f203052e78366401415931315bd99d9e3e281ae
kimhy <kimhyyefla@gmail.com>
2025-10-10 23:47:02 +0900

    feat: Define app-wide color constants

--- lib/week05/calendar_scheduler/const/colors.dart
+++ lib/week05/calendar_scheduler/const/colors.dart
@@ -1,1 +1,6 @@
 import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color(0xFF0DB2B2);
+final LIGHT_GREY_COLOR= Colors.grey[200]!;
+final DARK_GREY_COLOR= Colors.grey[600]!;
+final TEXT_FIELD_BORDER_COLOR= Colors.grey[300]!;
