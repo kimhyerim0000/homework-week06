504b59c6de21e0b72e9ed885b7f9d0b7f6de5fe9
kimhy <kimhyyefla@gmail.com>
2025-10-11 04:08:45 +0900

    feat(calendar_scheduler): Add TodayBanner widget to HomeScreen

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -30,6 +30,12 @@ class _HomeScreenState extends State<HomeScreen> {
               // 날짜가 선택됐을 때 실행할 함수
               onDaySelected: onDaySelected,
             ),
+            SizedBox(height: 8.0),
+            TodayBanner(
+              selectedDate: selectedDate,
+              count: 0,
+            ),
+            SizedBox(height: 8.0),
             ScheduleCard( // 구현해둔 일정 카드
               startTime: 12,
               endTime: 14,
