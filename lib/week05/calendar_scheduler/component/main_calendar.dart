d2bf101639f552133dd50328b0fd208c84297818
kimhy <kimhyyefla@gmail.com>
2025-10-10 23:48:49 +0900

    feat: Implement basic MainCalendar widget

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+class MainCalendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      firstDay
+    )
+  }
+}
