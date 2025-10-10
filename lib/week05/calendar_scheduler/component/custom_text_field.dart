8ac65df5f1aee67f9fa447471d36a94b4f9b9938
kimhy <kimhyyefla@gmail.com>
2025-10-11 04:29:59 +0900

    feat(calendar): Implement CustomTextField widget

--- lib/week05/calendar_scheduler/component/custom_text_field.dart
+++ lib/week05/calendar_scheduler/component/custom_text_field.dart
@@ -1,4 +1,18 @@
 import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 
-class CustomTextField 
+class CustomTextField extends StatelessWidget {
+  final String label;
+
+  const CustomTextField({
+    required this.label,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(  // 세로로 텍스트와 텍스트 필드 배치
+      
+    );
+  }
+}
