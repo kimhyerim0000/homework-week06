83955505586f95fbbb3377ed92f2a18fc39a3bcd
kimhy <kimhyyefla@gmail.com>
2025-10-11 03:44:45 +0900

    feat: Implement basic schedule card component

--- lib/week05/calendar_scheduler/component/schedule_card.dart
+++ lib/week05/calendar_scheduler/component/schedule_card.dart
@@ -1,3 +1,22 @@
 import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 
+class _Time extends StatelessWidget {
+  final int startTime; // 시작 시간
+  final int endTime;  // 종료 시간간
+
+  const _Time({
+    required this.startTime,
+    required this.endTime,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: PRI
+    )
+  }
+
+}
