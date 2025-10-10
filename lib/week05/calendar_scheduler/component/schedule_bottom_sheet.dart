89282bcbf46c5c99202313f1f8c55f3970f5a0c4
kimhy <kimhyyefla@gmail.com>
2025-10-11 04:34:24 +0900

    feat: Add basic CustomTextField to ScheduleBottomSheet

--- lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -15,6 +15,9 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
       child: Container(
         height: MediaQuery.of(context).size.height / 2,
         color: Colors.white,
+        child: CustomTextField(
+          label:'시작 시간',
+        ),
       ),
     );
   }
