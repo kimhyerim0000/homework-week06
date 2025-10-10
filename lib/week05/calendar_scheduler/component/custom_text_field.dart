413c3ca1b71ee2ad266a5daf251faf93f49b0481
kimhy <kimhyyefla@gmail.com>
2025-10-11 04:36:26 +0900

    feat: Add isTime parameter to CustomTextField

--- lib/week05/calendar_scheduler/component/custom_text_field.dart
+++ lib/week05/calendar_scheduler/component/custom_text_field.dart
@@ -1,11 +1,14 @@
 import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
+  final bool isTime;
 
   const CustomTextField({
     required this.label,
+    required this.isTime,
     Key? key,
   }) : super(key: key);
 
