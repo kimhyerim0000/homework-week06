797767039cebb6545a355f30cf9b17a2d683b7c0
kimhy <kimhyyefla@gmail.com>
2025-10-11 04:04:16 +0900

    feat: Implement TodayBanner widget with selectedDate and count properties

---
+++ lib/week05/calendar_scheduler/component/today_banner.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/week05/calendar_scheduler/const/colors.dart'; 
+
+class TodayBanner extends StatelessWidget {
+  final DateTime selectedDate;
+  final int count;
+
+  const TodayBanner({
+    required this.selectedDate,
+    required this.count,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
