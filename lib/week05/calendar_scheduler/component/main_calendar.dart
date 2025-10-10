a3713715b7154b45b1267cdcae09399d13ed2c9d
kimhy <kimhyyefla@gmail.com>
2025-10-11 00:02:49 +0900

    Fix: Corrected typo in HeaderStyle comment

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -9,7 +9,7 @@ class MainCalendar extends StatelessWidget {
       firstDay: DateTime(1800, 1, 1), // 첫째 날
       lastDay: DateTime(3000, 1, 1), // 마지막 날
       focusedDay: DateTime.now(), // 화면에 보여지는 날
-      headerStyle: HeaderStyle(
+      headerStyle: HeaderStyle(   /
         titleCentered: true,
         formatButtonVisible: false,
         titleTextStyle: TextStyle(
