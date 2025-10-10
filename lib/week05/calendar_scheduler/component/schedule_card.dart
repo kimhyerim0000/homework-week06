f8d523a1fb858420330e868d31d8d85a03afaba1
kimhy <kimhyyefla@gmail.com>
2025-10-11 03:52:45 +0900

    feat: Create ScheduleCard widget

--- lib/week05/calendar_scheduler/component/schedule_card.dart
+++ lib/week05/calendar_scheduler/component/schedule_card.dart
@@ -1,7 +1,9 @@
 import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 
-class
+class ScheduleCard extends StatelessWidget {
+  
+}
 
 
 class _Time extends StatelessWidget {
