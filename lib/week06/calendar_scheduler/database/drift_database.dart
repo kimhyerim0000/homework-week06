051b2f068cd053500e9f2e3dce7e4779a114cafa
kimhy <kimhyyefla@gmail.com>
2025-10-13 00:13:38 +0900

    feat: Initialize Drift database with Schedules table

---
+++ lib/week06/calendar_scheduler/database/drift_database.dart
@@ -0,0 +1,10 @@
+import 'package:myapp/week06/calendar_scheduler/model/schedule.dart';
+import 'package:drift/drift.dart';
+
+part 'drift_database.g.dart';
+
+@DriftDatabase(
+  tables: [
+    Schedules
+  ],
+)
