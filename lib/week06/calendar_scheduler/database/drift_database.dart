8b9b16bd05f437158430b5a98a75fee5bd13d305
kimhy <kimhyyefla@gmail.com>
2025-10-13 02:21:33 +0900

    feat: Add comments to Drift database code

--- lib/week06/calendar_scheduler/database/drift_database.dart
+++ lib/week06/calendar_scheduler/database/drift_database.dart
@@ -16,10 +16,11 @@ part 'drift_database.g.dart'; // part 파일 지정
 )
 
 class LocalDatabase extends _$LocalDatabase {
-
+  // Code Generation으로 생성할 클래스 상속속
   LocalDatabase() :super(_openConnection());
   
   Stream<List<Schedule>> watchSchedules(DateTime date) {
+   // 데이터를 조회하고 변화 감지
    return (select(schedules)..where((s) => s.date.equals(date))).watch();
   }
   Future<int> createSchedule(SchedulesCompanion data) =>
