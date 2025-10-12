1708529de61cac2c5d1f8de8c31f227ff56d0d10
kimhy <kimhyyefla@gmail.com>
2025-10-13 02:06:32 +0900

    Fix: Format schedule bottom sheet

--- lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -20,55 +20,56 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
   Widget build(BuildContext context) {
     // 키보드 높이 가져오기
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
-  return Form(
-    key: formKey,
-    child: SafeArea(
-      child: Container(
-        // 화면 반 높이에 키보드 높이 추가하기
-        height: MediaQuery.of(context).size.height / 2 + bottomInset,
-        color: Colors.white,
-        child: Padding(
-          padding: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: bottomInset), // 패딩에 키보드 높이 추가해서 위젯 전반적으로 위로 올려주기
-          child: Column(
-            // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로로 배치
-            children: [
-              Row(
-                // 시작 시간, 종료 시간 가로로 배치
-                children: [
-                  Expanded(
-                    child: CustomTextField( // 시작 시간 입력 필드
-                      label: '시작 시간',
-                      isTime: true,
+    return Form(
+      key: formKey,
+      child: SafeArea(
+        child: Container(
+          // 화면 반 높이에 키보드 높이 추가하기
+          height: MediaQuery.of(context).size.height / 2 + bottomInset,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: bottomInset), // 패딩에 키보드 높이 추가해서 위젯 전반적으로 위로 올려주기
+            child: Column(
+              // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로로 배치
+              children: [
+                Row(
+                  // 시작 시간, 종료 시간 가로로 배치
+                  children: [
+                    Expanded(
+                      child: CustomTextField( // 시작 시간 입력 필드
+                        label: '시작 시간',
+                        isTime: true,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 16.0),
-                  Expanded(
-                    child: CustomTextField( // 종료 시간 입력 필드
-                      label: '종료 시간',
-                      isTime: true,
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: CustomTextField( // 종료 시간 입력 필드
+                        label: '종료 시간',
+                        isTime: true,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8.0),
-              Expanded(
-                child: CustomTextField( // 내용 입력 필드
-                  label: '내용',
-                  isTime: false,
+                  ],
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: onSavePressed,
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: PRIMARY_COLOR,
+                SizedBox(height: 8.0),
+                Expanded(
+                  child: CustomTextField( // 내용 입력 필드
+                    label: '내용',
+                    isTime: false,
                   ),
-                  child: Text('저장'),
                 ),
-              ),
-            ],
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: onSavePressed,
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: PRIMARY_COLOR,
+                    ),
+                    child: Text('저장'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
