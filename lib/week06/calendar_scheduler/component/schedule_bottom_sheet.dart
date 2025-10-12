16cdc5e2cda9ee7e18e355068d7463ce034291c0
kimhy <kimhyyefla@gmail.com>
2025-10-13 02:11:01 +0900

    Updated Changes

--- lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -91,10 +91,10 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
     );
   }
 
-  void onSavePressed() {
+  void onSavePressed() {}
 
-    String? timeValidator(String? val) {}      // 시간값 검증
+  String? timeValidator(String? val) {}      // 시간값 검증
 
-    String? contentValidator(String? val) {}   // 내용값 검증
-  }
+  String? contentValidator(String? val) {}   // 내용값 검증
+  
 }
