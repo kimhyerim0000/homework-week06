6db0c4cd0cf81e36e1bdebd845f3da268a0f69b1
kimhy <kimhyyefla@gmail.com>
2025-10-13 02:02:32 +0900

    feat: Initialize Drift database and inject with GetIt

--- lib/week06/calendar_scheduler/main.dart
+++ lib/week06/calendar_scheduler/main.dart
@@ -1,6 +1,8 @@
 import 'package:myapp/week06/calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
@@ -8,6 +10,11 @@ void main() async {
 
   await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
 
+  final database = LocalDatabase(); // 데이터베이스 생성
+  
+  GetIt.I.registerSingleton<LocalDatabase>(database);
+  // GetIt에 데이터베이스 변수 주입하기
+  
   runApp(
     MaterialApp(
       home: HomeScreen(),
