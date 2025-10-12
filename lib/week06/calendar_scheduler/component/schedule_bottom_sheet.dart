da415f68bfa8c8fee4df6d835bc29d32b76c2b63
kimhy <kimhyyefla@gmail.com>
2025-10-13 02:06:02 +0900

    feat: Add schedule creation form and data storage fields

--- lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -10,12 +10,19 @@ class ScheduleBottomSheet extends StatefulWidget {
 }
 
 class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
+  final GlobalKey<FormState> formKey = GlobalKey(); // 폼 key 생성
+
+  int? startTime; // 시작 시간 저장 변수
+  int? endTime;   // 종료 시간 저장 변수
+  String? content; // 일정 내용 저장 변수
+
   @override
   Widget build(BuildContext context) {
     // 키보드 높이 가져오기
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
-
-    return SafeArea(
+  return Form(
+    key: formKey,
+    child: SafeArea(
       child: Container(
         // 화면 반 높이에 키보드 높이 추가하기
         height: MediaQuery.of(context).size.height / 2 + bottomInset,
