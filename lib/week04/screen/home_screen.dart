21bc8c45b02ecd60e46cc6ddce0df63f1dccbaf2
kimhy <kimhyyefla@gmail.com>
2025-10-13 10:17:19 +0900

    Updated Changes

---
+++ lib/week04/screen/home_screen.dart
@@ -0,0 +1,76 @@
+import 'package:myapp/week05/calender_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:myapp/week05/calender_scheduler/const/colors.dart';
+import 'package:myapp/week05/calender_scheduler/component/today _banner.dart';
+import 'package:myapp/week05/calender_scheduler/component/schedule_card.dart';
+import 'package:myapp/week05/calender_scheduler/component/main_calendar.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+// StatelessWidget에서 StatefulWidget으로 전환
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key : key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  DateTime selectedDate = DateTime.utc(     // 선택된 날짜를 관리할 변수
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(  
+
+      floatingActionButton: FloatingActionButton(     // 새 일정 버튼
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet(     // BottomSheet 열기
+            context: context,
+            isDismissible: true,    // 배경 탭 했을 때 BottomSheet 닫기
+            builder: (_) => ScheduleBottomSheet(),
+            // BottomSHeet의 높이를 화면의 최대 높이로 정의하고 스크롤 가능하게 변경
+            isScrollControlled: true,
+          );
+        },
+        child: Icon(  
+          Icons.add,
+        ),
+      ),
+
+      body: SafeArea(     // 시스템 UI 피해서 UI 구현하기
+        child: Column(    // 달력과 리스트를 세로로 배치
+          children: [  
+            // 미리 작업해둔 달력 위젯 보여주기
+            MainCalendar(
+              selectedDate: selectedDate,   // 선택된 날짜 전달하기              
+              onDaySelected: onDaySelected, // 날짜가 선택됐을 때 실행할 함수              
+            ),
+            SizedBox(height: 8.0,),
+            TodayBanner(      // 배너 추가하기
+              selectedDate: selectedDate,
+              count: 0,
+            ),
+            SizedBox(height: 8.0),
+            ScheduleCard(   // 구현해둔 일정 카드
+              startTime: 12,
+              endTime: 14,
+              content: '프로그래밍 공부',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    // 날짜 선택될 때마다 실행할 함수
+    setState(() { 
+      this.selectedDate = selectedDate;
+    });
+  }
+}
