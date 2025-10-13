747c151c96dbf8c968e48b8e7e86ef2cbf423ce8
kimhy <kimhyyefla@gmail.com>
2025-10-13 11:05:59 +0900

    Updated Changes

--- lib/week04/calender_scheduler/database/drift_database.dart
+++ lib/week04/calender_scheduler/database/drift_database.dart
@@ -2,3 +2,9 @@ import 'package:myapp/week04/calender_scheduler/model/schedule.dart';
 
 part 'drift_database.g.dart';
 
+@DriftDatabase(
+  tables: [
+    Schedules,
+  ]
+)
+class LocalDatabase extends _$LocalDatabase {}
